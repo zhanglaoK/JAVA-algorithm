5dce69cc09d725a9201eaef657965ab63f79d472
1142030350 <1142030350@qq.com>
2019-11-04 15:44:22 +0800

    增加动态规划的解法

--- src/com/junxi/day08/MinPath.java
+++ src/com/junxi/day08/MinPath.java
@@ -21,12 +21,13 @@ public class MinPath {
             return res;
         }
         if(i==0 && j!=0){
-            return res + minPath(matrix,i,j+1);
+            return res + minPath(matrix,i,j-1);
         }
         if(i!=0 && j==0){
-            return res + minPath(matrix,i+1,j);
+            return res + minPath(matrix,i-1,j);
         }
-        return res + Math.min(minPath(matrix,i+1,j),minPath(matrix,i,j+1));
+
+        return res + Math.min(minPath(matrix,i-1,j),minPath(matrix,i,j-1));
     }
 
     /**
@@ -37,18 +38,53 @@ public class MinPath {
      * @return
      */
     public static int minPath1(int[][]matrix,int i,int j){
-        if(i==matrix.length-1 && j==matrix[0].length){
+        if(i==matrix.length-1 || j==matrix[0].length){
             return matrix[i][j];
         }
         if(i==matrix.length-1){
-            return matrix[i][j] + minPath(matrix,i,j+1);
+            return matrix[i][j] + minPath1(matrix,i,j+1);
         }
         if(j==matrix[0].length-1){
-            return matrix[i][j] + minPath(matrix,i+1,j);
+            return matrix[i][j] + minPath1(matrix,i+1,j);
         }
-        return matrix[i][j] + Math.min(minPath(matrix,i+1,j),minPath(matrix,i,j+1));
+        return matrix[i][j] + Math.min(minPath1(matrix,i+1,j),minPath1(matrix,i,j+1));
     }
 
 
+    /**
+     * 动态规划解法
+     * @param matrix
+     * @return
+     */
+    public static int minPath2(int[][]matrix){
+        if(matrix == null || matrix.length == 0 || matrix[0] == null || matrix[0].length == 0){
+            return 0;
+        }
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int [][]dp = new int [row][col];
+        dp[0][0] = matrix[0][0];
+        for(int i=1;i<row;i++){
+            dp[i][0] = dp[i-1][0] + matrix[i][0];
+        }
+        for (int j=1;j<col;j++){
+            dp[0][j] = dp[0][j-1] + matrix[0][j];
+        }
+        for(int i=1;i<row;i++){
+            for(int j=1;j<col;j++){
+                dp[i][j] = Math.min(dp[i-1][j],dp[i][j-1]) + matrix[i][j];
+            }
+        }
+        return dp[row-1][col-1];
+    }
+
+
+    public static void main(String[] args) {
+        int[][] m = { { 1, 3, 5, 9 }, { 8, 1, 3, 4 }, { 5, 0, 6, 1 }, { 8, 8, 4, 0 } };
+        System.out.println(minPath(m,m.length-1,m[0].length-1));
+        System.out.println(minPath1(m,0,0));
+        System.out.println(minPath2(m));
+
+    }
 
 }
